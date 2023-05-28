3488a9e7c54526848cad547ae77e9fe8c2e0cee4
David Zemlin <dzemlin@asu.edu>
2023-05-28 17:49:25 -0400

    fix total

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -318,7 +318,7 @@ class WebServer {
             builder.append("HTTP/1.1 200 OK\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
-            builder.append("Result is: " + result);
+            builder.append("Total is: " + sum);
             } catch (NumberFormatException f){
                 builder.append("HTTP/1.1 400 Bad Request\n");
                 builder.append("Content-Type: text/html; charset=utf-8\n");
